0100e2e397222c9647dc6dc618973babb85e077d
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-06-15 18:47:06 +0800

    add our team compnent

--- src/components/OurTeam/index.jsx
+++ src/components/OurTeam/index.jsx
@@ -13,17 +13,18 @@ export default function OurTeam() {
   const navigationNextRef = useRef(null);
 
   return (
-    <div className="bg-tertiary-300">
+    <div className="bg-tertiary-300" id="team">
       <div className="flex flex-wrap justify-center mb-10 text-center">
         <div className="w-full px-4 lg:w-6/12">
           <h1 className="text-[2rem] md:text-[2.5rem] lg:text-[50px] font-medium mb-[18px]  text-primary-100 font-head">
-            Our Incredible Team
+            Ini Kami, BarKit Team
           </h1>
           <p className="text-sm font-body text-black-400">
-            Bringing together a wealth of expertise spanning over a century, our
-            team is a force to be reckoned with. As an addition, we proudly
-            include the talented individuals from Bangkit 2023, who bring fresh
-            perspectives and cutting-edge skills to our dynamic team.
+            Dengan keahlian dan pengalaman yang luas, kami bekerja sebagai tim
+            yang kuat untuk memberikan hasil luar biasa kepada klien kami. Kami
+            bangga menyertakan anggota berbakat dari Bangkit 2023 yang membawa
+            keterampilan terkini. Bersama, kami menciptakan lingkungan kerja
+            dinamis, kolaboratif, dan kreatif.
           </p>
         </div>
       </div>
@@ -42,14 +43,53 @@ export default function OurTeam() {
             className="relative"
           >
             <SwiperSlide>
-              <OurTeamPart />
+              <OurTeamPart
+                imageSrc="/thoriq.jpg"
+                subHead="Muh Thoriq Ali Said"
+                paragraph="Saya adalah seorang Cloud Engineer & Web Developer di tim BarKit serta menjadi anggota Cloud Computing Cohort di Bangkit 2023. Sebagai seorang Cloud Engineer, tugas saya adalah merancang, mengimplementasikan, dan mengelola lingkungan cloud yang aman dan skalabel untuk proyek tim BarKit. Sebagai seorang Web Developer, saya bertanggung jawab untuk mengembangkan aplikasi web yang responsif, fungsional, dan menarik bagi pengguna"
+              />
             </SwiperSlide>
             <SwiperSlide>
-              <OurTeamPart />
+              <OurTeamPart
+                imageSrc="/dian.jpg"
+                subHead="Dian Ayu Rahmawati"
+                paragraph="Saya adalah seorang UI/UX di tim BarKit dan juga merupakan Cloud Computing Cohort di Bangkit 2023. Sebagai UI/UX Designer, tugas utama saya adalah merancang antarmuka pengguna yang menarik dan intuitif untuk aplikasi kami. Saya bertanggung jawab untuk menciptakan pengalaman pengguna yang baik dengan menggabungkan desain yang estetis dan fungsionalitas yang optimal"
+                role="UI/UX"
+              />
             </SwiperSlide>
             <SwiperSlide>
-              <OurTeamPart />
+              <OurTeamPart
+                imageSrc="/robby.jpg"
+                subHead="Robby Alamsyah"
+                paragraph="Saya adalah seorang Mobile Developer di tim BarKit dan juga merupakan Mobile Developer Cohort di Bangkit 2023. Sebagai Mobile Developer, tugas utama saya adalah merancang dan mengembangkan aplikasi mobile yang responsif dan fungsional. Saya bertanggung jawab untuk menciptakan pengalaman pengguna yang baik dengan antarmuka yang menarik dan performa yang optimal"
+                role="Mobile Developer"
+              />
             </SwiperSlide>
+            <SwiperSlide>
+              <OurTeamPart
+                imageSrc="/awang.jpg"
+                subHead="Awang Mulya Nugrawan"
+                paragraph="Saya adalah seorang Machine Learning Engineer di tim BarKit dan juga merupakan anggota Machine Learning Cohort di Bangkit. Sebagai Machine Learning Engineer, tugas utama saya adalah merancang, mengembangkan, dan menerapkan model machine learning untuk memecahkan masalah dan mengambil wawasan berharga dari data. Saya bertanggung jawab untuk melakukan pra-pemrosesan data, memilih dan melatih model yang tepat, serta mengevaluasi kinerjanya"
+                role="Machine Learning Engineer"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <OurTeamPart
+                imageSrc="/illa.jpg"
+                subHead="Andi Illa Erviani Nensi"
+                paragraph="Saya adalah seorang Machine Learning Engineer di tim BarKit dan juga merupakan anggota Machine Learning Cohort di Bangkit. Sebagai Machine Learning Engineer, tugas utama saya adalah merancang, mengembangkan, dan menerapkan model machine learning untuk memecahkan masalah dan mengambil wawasan berharga dari data. Saya bertanggung jawab untuk melakukan pra-pemrosesan data, memilih dan melatih model yang tepat, serta mengevaluasi kinerjanya"
+                role="Machine Learning Engineer"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <OurTeamPart
+                imageSrc="/naufal.jpg"
+                subHead="Naufal Mahardika Putra"
+                paragraph="Saya adalah seorang Machine Learning Engineer di tim BarKit dan juga merupakan anggota Machine Learning Cohort di Bangkit. Sebagai Machine Learning Engineer, tugas utama saya adalah merancang, mengembangkan, dan menerapkan model machine learning untuk memecahkan masalah dan mengambil wawasan berharga dari data. Saya bertanggung jawab untuk melakukan pra-pemrosesan data, memilih dan melatih model yang tepat, serta mengevaluasi kinerjanya"
+                role="Machine Learning Engineer"
+              />
+            </SwiperSlide>
+
             <div className="absolute bottom-0 right-0 z-10 flex items-center gap-6 sm:right-10">
               <button
                 className="testimonial-button prev"
--- src/components/OurTeam/ourTeamPart.jsx
+++ src/components/OurTeam/ourTeamPart.jsx
@@ -1,27 +1,19 @@
 import SubHead from '../Atoms/subhead';
 import Paragraph from '../Atoms/paragraph';
 
-export default function OurTeamPart() {
+export default function OurTeamPart({ imageSrc, subHead, paragraph, role }) {
   return (
     <div className="flex flex-col gap-[50px] items-center md:flex-row md:items-end lg:gap-44 lg:items-center">
       <img
         className="w-[300px] md:w-8/12 lg:w-2/5"
-        src="/profile3.jpg"
+        src={imageSrc}
         alt="testimonial"
       />
       <div className="flex flex-col sm:w-10/12">
-        <SubHead style="mb-[18px] w-9/12 md:w-full">
-          Muhamad Thoriq Ali Said
-        </SubHead>
-        <Paragraph style="mb-9">
-          “Lorem ipsum dolor sit amet consectetur adipiscing elit turpis viverra
-          amet elit est proin tgestas neque quis aliq vel. Viverra gravida orci
-          vitae at aliquam sit accums rutrum ut convallis.”
-        </Paragraph>
+        <SubHead style="mb-[18px] w-9/12 md:w-full">{subHead}</SubHead>
+        <Paragraph style="mb-9">{paragraph}</Paragraph>
         <div className="flex flex-col font-body">
-          <h5 className="text-primary-100 mb-2.5 font-medium">
-            BackEnd Developer{' '}
-          </h5>
+          <h5 className="text-primary-100 mb-2.5 font-medium">{role}</h5>
           <p className="text-black-300">BarKit Indonesia</p>
         </div>
       </div>
